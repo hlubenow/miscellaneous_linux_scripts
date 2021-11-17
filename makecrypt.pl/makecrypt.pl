32c9867166da717a33704696082ba2574f30deac
Hauke Lubenow <83097630+hlubenow@users.noreply.github.com>
2021-11-17 02:54:40 +0100

    Update makecrypt.pl

--- makecrypt.pl/makecrypt.pl
+++ makecrypt.pl/makecrypt.pl
@@ -11,7 +11,7 @@ use Cwd;
                        on certain Linux distributions.
                        Works for me on OpenSuSE Linux 13.1 (32bit).
 
-    Copyright (C) 2021 Hauke Lubenow, Germany
+    Copyright (C) 2021, hlubenow
 
     This program is free software: you can redistribute it and/or modify
      it under the terms of the GNU General Public License as published by
